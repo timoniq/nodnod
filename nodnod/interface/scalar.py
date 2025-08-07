4cf6b9f851ce136dbda026342f9806289f6ddf6b
timoniq <arseny.kriuchkov@gmail.com>
2025-08-07 16:13:11 +0400

    add scalar node from func

--- nodnod/interface/scalar.py
+++ nodnod/interface/scalar.py
@@ -1,4 +1,5 @@
 import typing
+import inspect
 
 from nodnod.node import Node
 from nodnod.utils.create_node import create_node
@@ -23,9 +24,22 @@ class scalar_node[T]:  # noqa: N801
     def __new__(cls, x: Composable[T], /) -> type[T]: ...
 
     @typing.overload
+    def __new__(cls, x: typing.Callable[..., typing.Awaitable[T]], /) -> type[T]: ...
+
+    @typing.overload
+    def __new__(cls, x: typing.Callable[..., Generator[T]], /) -> type[T]: ...
+
+    @typing.overload
+    def __new__(cls, x: typing.Callable[..., T], /) -> type[T]: ...
+
+    @typing.overload
     def __new__(cls, x: T, /) -> type[T]: ...
 
     def __new__(cls, node_class: typing.Any, /) -> typing.Any:
+
+        if inspect.isfunction(node_class):
+            node_class = type(node_class.__name__, (), dict(__compose__=node_class))
+
         if not any(issubclass(base, Node) for base in node_class.__bases__):
             return create_node(
                 name=node_class.__name__,
