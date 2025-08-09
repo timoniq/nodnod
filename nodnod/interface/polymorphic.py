8f822487339332ac0057b930d86b3cff8053d35c
timoniq <arseny.kriuchkov@gmail.com>
2025-08-09 19:44:24 +0400

    polymorphic node type creation fix

--- nodnod/interface/polymorphic.py
+++ nodnod/interface/polymorphic.py
@@ -58,7 +58,7 @@ class polymorphic[T]:  # noqa: N801
         return create_node(
             name=node_class.__name__,
             base_node=cls.POLYMORPHIC_NODE_CLS,
-            bases=tuple(),
+            bases=(node_class,),
             namespace=dict(__either__=tuple(case_nodes)),
         )
 
