aeb01f4d17ab6e0ad021128fc88353f393b5ac53
Georgy Pliev <howluwqz1@gmail.com>
2025-12-24 16:34:03 +0300

    feat: add resolve_callable_annotations

--- nodnod/utils/resolve_signature.py
+++ nodnod/utils/resolve_signature.py
@@ -1,15 +1,18 @@
 import dataclasses
 import inspect
+import sys
 import typing
 from functools import cache
+
+from typing_extensions import Format, evaluate_forward_ref, get_annotations
 
 type AnnotationForm = typing.Any | typing.ForwardRef
 
 
 @dataclasses.dataclass
 class Signature:
-    args: dict[str, type[typing.Any] | typing.ForwardRef]
-    kwargs: dict[str, type[typing.Any] | typing.ForwardRef]
+    args: dict[str, AnnotationForm]
+    kwargs: dict[str, AnnotationForm]
     var_positional: inspect.Parameter | None = None
     var_keyword: inspect.Parameter | None = None
 
@@ -22,15 +25,43 @@ class Signature:
         return self.var_keyword.annotation if self.var_keyword else None
 
     def get_all_types(self) -> set[AnnotationForm]:
-        types = set[type[typing.Any] | typing.ForwardRef]()
+        types: set[AnnotationForm] = set()
+
         for t in self.args.values():
             types.add(t)
+
         for t in self.kwargs.values():
             types.add(t)
+
         return types
-    
-    def merge(self) -> dict[str, typing.Any]:
+
+    def merge(self) -> dict[str, AnnotationForm]:
         return self.args | self.kwargs
+
+
+@cache
+def resolve_callable_annotations(obj: typing.Callable[..., typing.Any], /) -> dict[str, AnnotationForm]:
+    """Resolves callable annotations"""
+
+    annotations: dict[str, AnnotationForm] = {}
+
+    for name, annotation in get_annotations(obj, eval_str=False, format=Format.FORWARDREF).items():
+        if isinstance(annotation, str):
+            module = sys.modules.get(obj_mod, None) if (obj_mod := getattr(obj, "__module__", None)) is not None else None
+            annotation = typing.ForwardRef(arg=annotation, is_argument=True, module=module)
+
+        if not isinstance(annotation, typing.ForwardRef):
+            annotations[name] = annotation
+            continue
+
+        try:
+            value = evaluate_forward_ref(annotation, owner=obj, format=Format.VALUE)
+        except NameError:
+            annotations[name] = annotation
+        else:
+            annotations[name] = value
+
+    return annotations
 
 
 @cache
@@ -41,7 +72,7 @@ def resolve_signature(callable: typing.Callable[..., typing.Any], ignore_bound_p
         callable = callable.__func__  # type: ignore
 
     sig = inspect.signature(callable)
-    hints = callable.__annotations__
+    hints = resolve_callable_annotations(callable)
     args = {}
     kwargs = {}
     var_positional = None
@@ -60,19 +91,15 @@ def resolve_signature(callable: typing.Callable[..., typing.Any], ignore_bound_p
             continue
 
         typ = hints.get(name, typing.Any)
-
-        if isinstance(typ, str):
-            typ = typing.ForwardRef(typ)
-
         if param.default is param.empty and param.kind is param.POSITIONAL_ONLY:
             args[name] = typ
         else:
             kwargs[name] = typ
 
     return Signature(
-        args=args, 
-        kwargs=kwargs, 
-        var_positional=var_positional, 
+        args=args,
+        kwargs=kwargs,
+        var_positional=var_positional,
         var_keyword=var_keyword,
     )
 
