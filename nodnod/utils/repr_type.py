a00cf0d5b1e56ad6d8ebace38c43ded26abe67c2
Georgy Pliev <howluwqz1@gmail.com>
2026-01-22 03:13:54 +0300

    change str() to repr() for non-type objects

--- nodnod/utils/repr_type.py
+++ nodnod/utils/repr_type.py
@@ -2,7 +2,7 @@ import typing
 
 
 def type_repr(obj: typing.Any, /) -> str:
-    return obj.__name__ if isinstance(obj, type) else str(obj)
+    return obj.__name__ if isinstance(obj, type) else repr(obj)
 
 
 __all__ = ("type_repr",)
